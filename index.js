caf4d00aa3fed6bbf1a36252d76e5209940b96ba
Lam Nguyen <l.nguyen@celayix.com>
2019-04-20 14:38:19 -0700

    Basic setup

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+"use-strict";
 var express = require("express");
 var app = express();
+var path = require("path");
 var bodyParser = require("body-parser");
 var port = 3000;
 
-
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get("/", (req, res, next)=>{
+	res.send("Hello world!");
+})
 app.listen(port, () => {
 	console.log("Server is listening on port + ", port);
 })
